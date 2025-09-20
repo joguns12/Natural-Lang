36a8a56724322792e7fd30fe9e9eef42758c4510
joguns12 <111800324+joguns12@users.noreply.github.com>
2025-09-20 04:43:48 -0500

    canvas files

--- CorpusReader_SLM.py
+++ CorpusReader_SLM.py
@@ -1,1 +1,26 @@
-print("test")
+''' 
+Add modules you want to import first
+'''
+
+class CorpusReader_SLM:
+    def __init__(self, corpus, stopWord = 'none', toStem = False,  smooth = False, trigram = False):
+        count = 0
+
+    def unigram(self, count = 0):
+        return []
+
+    def bigram(self, count = 0):
+        return []
+
+    def trigram(self, count = 0):
+        return []
+
+    def unigramGenerate(self, code=0, head=[]):
+        return ""
+
+    def bigramGenerate(self, code=0, head=[]):
+        return ""
+
+    def trigramGenerate(self, code=0, head=[]):
+        return ""
+
